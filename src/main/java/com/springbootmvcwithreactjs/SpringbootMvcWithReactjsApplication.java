60ede4963a880f6b7fc1da9a6417b5e5c74b3c08
sushil <sushilsridhar16@gmail.com>
2021-06-12 07:54:53 +0530

    project creation

---
+++ src/main/java/com/springbootmvcwithreactjs/SpringbootMvcWithReactjsApplication.java
@@ -0,0 +1,13 @@
+package com.springbootmvcwithreactjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMvcWithReactjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMvcWithReactjsApplication.class, args);
+	}
+
+}
