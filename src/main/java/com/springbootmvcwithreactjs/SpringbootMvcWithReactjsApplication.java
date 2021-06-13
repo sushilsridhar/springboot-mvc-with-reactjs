d0c1e7016bffc2365dadfc5e391f81081cecb41c
sushil <sushilsridhar16@gmail.com>
2021-06-13 11:47:55 +0530

    added comments

--- src/main/java/com/springbootmvcwithreactjs/SpringbootMvcWithReactjsApplication.java
+++ src/main/java/com/springbootmvcwithreactjs/SpringbootMvcWithReactjsApplication.java
@@ -8,6 +8,8 @@ import org.springframework.web.SpringServletContainerInitializer;
 @SpringBootApplication
 public class SpringbootMvcWithReactjsApplication extends SpringBootServletInitializer {
 
+	/* SpringBootServletInitializer - Initializes Servlet 'dispatcherServlet' */
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootMvcWithReactjsApplication.class, args);
 	}
